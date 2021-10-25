e3f7af8bac748c364927842d2c57d9910f661325
Tim Yang <tyang098@gmail.com>
2021-10-25 01:20:28 -0400

    adding prospective libraries

---
+++ mysh.c
@@ -0,0 +1,16 @@
+// Tim Yang
+// COP 4600 - OS
+// Shell program called Mysh
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main(void)
+{
+    printf("hello\n");
+}
